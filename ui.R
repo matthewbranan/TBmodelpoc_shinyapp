3ce220f1af9c6b1dc73203729af596d7113d0d7e
bobavenger1 <bobavenger1@hotmail.com>
2015-12-17 14:57:00 -0700

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,43 +1,31 @@
 library(shiny)
 
 
-#Define a UI structure
+# Define a UI structure
 shinyUI(pageWithSidebar(
 
-	#App title
-	headerPanel("Distribution Shapes and QQ Plots"),
+	# App title
+	headerPanel("Quick-Calculator for Scenario Tree Prior Probability"),
 	
 	sidebarPanel(
-		#Choose what general type of distribution to plot
-		selectInput("distrshape", "Distribution Shape:", #Let the user choose from a variety of different distribution shapes
-			list("Normal"="normal",
-				"Skewed Left"="skewedleft",
-				"Skewed Right"="skewedright",
-				"Heavy Tailed"="heavytailed",
-				"Short Tailed"="shorttailed",
-				"Bimodal"="bimodal",
-				"Multimodal"="multimodal"
-		)),
+		# Choose sample size
+		numericInput("n", "Sample Size:", 30), # Have the user select the sample size. Be careful, no error checking for, possibly infinite or at least, very large sample s	
+
+		# Choose design prevalence
+		numericInput("dp", "Design Prevalence:", 0.05),
+
+		# Go Button
+		actionButton("goButton", "Calculate") # Have a button that will re-run the calculations upon request
 		
-	conditionalPanel( #Add a slider bar for user to choose the number of modes in the multimodal distribution
-		condition="distershape==multimodal",
-		sliderInput("modes","Modes (for Multimodal option only):",
-			min=3,max=8,value=3)
-		),
+		# Return the estimated prior probablity
+		numericOutput("output$priorprob_calc")
 		
-		
-		#Choose sample size
-		numericInput("n","Sample Size:",1000), #Have the user select the sample size. Be careful, no error checking for, possibly infinite or at least, very large sample sizes
-		
-		#Go Button
-		actionButton("goButton","Randomize!") #Have a button that will re-select the random sample from the distribution specified
 		),
 		
 	
 	#Display the plot and caption as input by user
-	mainPanel(	#Plot the two graphs side-by-side as square items in the main display space
-		div(class="span6",plotOutput("histogram",width="400px",height="400px")),
-		div(class="span6",plotOutput("QQplot",width="400px",height="400px"))
+	mainPanel(	#Plot the recommended prior as functions of sample size and design prevalence
+		div(class="span6",plotOutput("priorprob_plot",width="400px",height="400px")),
 	)
 	
 ))
