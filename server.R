499d0585f4b8e220db3bede37f4873e1064c7066
bobavenger1 <bobavenger1@hotmail.com>
2015-12-17 14:59:53 -0700

    Update server.R

--- server.R
+++ server.R
@@ -4,13 +4,13 @@ library(shiny)
 shinyServer(function(input, output){
 	
 	output$priorprob_calc = reactive({
-		input$goButton #Make the distribution randomize upon the go button
+		# input$goButton #Make the distribution randomize upon the go button
 		priorprob = (1 - (1 - (1 - input$dp)^input$n)) / (1 - (1 - (1 - input$dp)^input$n)^2)
 	})
 	
 	
 	output$priorprob_plot = renderPlot({
-		input$goButton #Re-render this plot as well with go button
+		# input$goButton #Re-render this plot as well with go button
 		plot(1:100, (1 - (1 - (1 - input$dp)^1:100)) / (1 - (1 - (1 - input$dp)^1:100)^2), type = "l")
 		points(input$n, output$priorprob_calc, pch = 8, col = "red")
 	})
