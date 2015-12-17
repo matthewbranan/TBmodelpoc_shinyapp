7fa3c330ebafba4ba7af793ee15258ba9360d3df
bobavenger1 <bobavenger1@hotmail.com>
2015-12-17 15:53:54 -0700

    Update server.R

--- server.R
+++ server.R
@@ -4,13 +4,16 @@ library(shiny)
 shinyServer(function(input, output){
 
 	priorprob_sse = reactive({
-		sse = 1 - (1 - input$dp)^1:100
+		sse = 1 - (1 - input$dp)^c(1:100)
 		})
 		
 	priorprob_calc = reactive({
 		calc = (1 - priorprob_sse()) / (1 - priorprob_sse()^2)
 		})
 		
+	priorprob_out = reactive({
+		out = (1 - (1 - (1 - input$dp)^input$n)) / (1 - (1 - (1 - input$dp)^input$n)^2)
+		})
 
 	output$priorprob_plot <- renderPlot({
 		plot(1:100, priorprob_calc(), type = "l")
