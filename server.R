63d2b7cb17472acb7ea2c052593a8a1d00bbc496
bobavenger1 <bobavenger1@hotmail.com>
2015-12-17 16:18:19 -0700

    Update server.R

--- server.R
+++ server.R
@@ -4,7 +4,7 @@ library(shiny)
 shinyServer(function(input, output){
 		
 	priorprob_calc = reactive({
-		calc = (1 - (1 - (1 - input$dp)^c(input$minplot:input$maxplot))) / (1 - (1 - (1 - input$dp^c(input$minplot:input$maxplot))^2))
+		calc = (1 - (1 - (1 - input$dp)^c(input$minplot:input$maxplot))) / (1 - (1 - (1 - input$dp)^c(input$minplot:input$maxplot)^2))
 		})
 		
 	priorprob_out = reactive({
@@ -12,7 +12,7 @@ shinyServer(function(input, output){
 		})
 
 	output$priorprob_plot <- renderPlot({
-		plot(input$minplot:input$maxplot, priorprob_calc(), type = "l", main = "Desired prior probability of infection for prior prevalence distribution Uniform(0, 1)", ylab = "Desired prior probability", xlab = "Design sample size")
+		plot(input$minplot:input$maxplot, priorprob_calc(), type = "l", main = "Desired prior probability of infection for \n prior prevalence distribution Uniform(0, 1)", ylab = "Desired prior probability", xlab = "Design sample size")
 		points(input$n, priorprob_out(), pch = 8, cex = 2, col = "red")
 		legend("topright", legend = c("Estimated uninformative prior probability"), pch = 8, col = "red")
 		})
