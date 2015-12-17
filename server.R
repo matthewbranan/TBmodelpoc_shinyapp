02f53d394ac9c3c368b30575ca98a584ef5dc39a
bobavenger1 <bobavenger1@hotmail.com>
2015-12-17 16:21:09 -0700

    Update server.R

--- server.R
+++ server.R
@@ -13,8 +13,8 @@ shinyServer(function(input, output){
 
 	output$priorprob_plot <- renderPlot({
 		plot(input$minplot:input$maxplot, priorprob_calc(), type = "l", main = "Desired prior probability of infection for \n prior prevalence distribution Uniform(0, 1)", ylab = "Desired prior probability", xlab = "Design sample size")
-		points(input$n, priorprob_out(), pch = 8, cex = 2, col = "red")
 		grid()
+		points(input$n, priorprob_out(), pch = 8, cex = 2, col = "red")
 		legend("topright", legend = c("Estimated uninformative prior probability"), pch = 8, col = "red")
 		})
 		
