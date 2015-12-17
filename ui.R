cb5de8f0cf509612869d910bb442a2303d370676
bobavenger1 <bobavenger1@hotmail.com>
2015-12-17 15:23:00 -0700

    Update ui.R

--- ui.R
+++ ui.R
@@ -5,27 +5,19 @@ library(shiny)
 shinyUI(pageWithSidebar(
 
 	# App title
-	headerPanel("Quick-Calculator for Scenario Tree Prior Probability"),
+	headerPanel("Temptitle"),
 	
 	sidebarPanel(
-		# Choose sample size
-		numericInput("n", "Sample Size:", 30), # Have the user select the sample size. Be careful, no error checking for, possibly infinite or at least, very large sample s	
-
-		# Choose design prevalence
-		numericInput("dp", "Design Prevalence:", 0.05),
-
-		# Go Button
-		# actionButton("goButton", "Calculate") # Have a button that will re-run the calculations upon request
-		
-		# Return the estimated prior probablity
-		# numericOutput(output$priorprob_calc)
-		
+		sliderInput("obs", 
+			"Number of Observations:",
+			min = 1,
+			max = 1000,
+			value = 500)
 		),
 		
+	mainPanel(
+		plotOutput("distPlot")
+		)
 	
-	#Display the plot and caption as input by user
-	mainPanel(	#Plot the recommended prior as functions of sample size and design prevalence
-		plotOutput("priorprob_plot")
-	)
 	 
 ))
