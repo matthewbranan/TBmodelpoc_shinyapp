ce1c2b4539bad5a491f4db8e603d0d2459df2fc6
matthewbranan <bobavenger1@hotmail.com>
2015-12-17 16:26:26 -0700

    Update ui.R

--- ui.R
+++ ui.R
@@ -8,10 +8,13 @@ shinyUI(pageWithSidebar(
 	headerPanel("Quick-calculator for scenario tree prior probability"),
 	
 	sidebarPanel(
+		# Include input for the (design) sample size
 		numericInput("n", "Sample size:", value = 30),
 		
+		# Include input for the desing prevalence
 		numericInput("dp", "Design prevalence:", value = 0.02),
 		
+		# Would like to give user some contorl over the (x-axis) plotting window
 		numericInput("minplot", "Minimum sample size in plot:", value = 1),
 		
 		numericInput("maxplot", "Maximum sample size in plot:", value = 100)
@@ -19,9 +22,11 @@ shinyUI(pageWithSidebar(
 		),
 		
 	mainPanel(
+		# Output the estimated uninformed prior probability
 		verbatimTextOutput("text"),
 		
-		plotOutput("priorprob_plot")
+		# Plot the results just like in the paper, keeping the plotting window at a standard size
+		div(class = "span6", plotOutput("priorprob_plot", width = "500px", height = "500px"))
 		
 		)
 	
