bc053d1f1ee5ad63d3b2a04edc39dab2feb2745a
matthewbranan <bobavenger1@hotmail.com>
2016-03-30 16:48:23 -0600

    Update server.R

--- server.R
+++ server.R
@@ -97,7 +97,7 @@ shinyServer(function(input, output){
 		local({
 			my_i = i
 			plotname = c("eta", "theta", "pi")[i]
-			temp_acf = acf(jagsamp_df()[, plotname], plot = FALSE)
+			temp_acf = acf(jagsamp_df()[[plotname]], plot = FALSE)
 			temp_acfdf = with(temp_acf, data.frame(lag, acf))
 			output[[paste0("acfplot_", plotname)]] = renderPlot({
 				ggplot(data = temp_acfdf, mapping = aes(x = lag, y = acf)) + geom_hline(aes(yintercept = 0)) + geom_segment(mapping = aes(xend = lag, yend = 0))
