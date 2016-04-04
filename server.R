7288706e837da0700c76ee9af1bae5b15c3a8518
matthewbranan <bobavenger1@hotmail.com>
2016-04-04 09:09:11 -0600

    Update server.R

--- server.R
+++ server.R
@@ -85,7 +85,7 @@ shinyServer(function(input, output){
 		
 ## Creating outputs
 	# HPD intervals output
-	output$summary_hpdout = renderDataTable({
+	output$summary_hpdout = renderTable({
 		round(HPDinterval(jagsamp_out(), 0.95)[[1]], input$digits)
 		})  # , digits = 16
 	
