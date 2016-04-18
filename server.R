e2ec8a8777d8a550aab1941af292221d271e0708
matthewbranan <bobavenger1@hotmail.com>
2016-04-18 16:12:30 -0600

    Update server.R

--- server.R
+++ server.R
@@ -85,15 +85,15 @@ shinyServer(function(input, output){
 		
 ## Creating outputs
 	# HPD intervals output
-	output$summary_hpdout = renderTable({
+	output$summary_hpdout = reactive({renderTable({
 		round(HPDinterval(jagsamp_out(), 0.95)[[1]], input$digits)
-		}, input$digits) 
+		}, digits = input$digits) )}
 	
 	# Summary statistics output
-	output$summary_sumstatout = renderTable({
+	output$summary_sumstatout = reactive({renderTable({
 		temp_sumstatout = summary(jagsamp_out())
 		round(cbind(temp_sumstatout[[1]][, c(1, 2, 4)], temp_sumstatout[[2]]), input$digits)
-		}, input$digits)
+		}, digits = input$digits)})
 		
 	# Diagnostic plots
 	
